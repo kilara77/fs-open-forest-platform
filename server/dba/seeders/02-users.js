a475ed7a7b730ffe43d3ba49cbaadcd31ce44df9
brian davidson <brian.davidson.1981@gmail.com>
2019-09-12 21:26:24 -0400

    689 - fleshed out temporary seed data for easier dev

--- server/dba/seeders/02-users.js
+++ server/dba/seeders/02-users.js
@@ -2,9 +2,25 @@ const users = [
   {
     id: 1,
     name: 'Brian Davidson',
-    role: 'user',
+    role: 'admin',
     email: 'bdavidson@cynerge.com',
     forests: ['{"name": "forest1", "access": "write"}', '{"name": "forest2", "access": "read"}'],
+    created: 'now()',
+    updated: 'now()'
+  }, {
+    id: 2,
+    name: 'Mike Laney',
+    role: 'admin',
+    email: 'mlaney@cynerge.com',
+    forests: ['{"name": "forest1", "access": "write"}', '{"name": "forest2", "access": "read"}', '{"name": "forest3", "access": "read"}', '{"name": "forest4", "access": "read"}'],
+    created: 'now()',
+    updated: 'now()'
+  }, {
+    id: 3,
+    name: 'Test User',
+    role: 'user',
+    email: 'test@test.com',
+    forests: ['{"name": "forest8", "access": "read"}'],
     created: 'now()',
     updated: 'now()'
   }
